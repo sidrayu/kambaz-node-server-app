3322411caa9bfea21586d9bb199746d5cb319a49
sidrayu <sidra@udel.edu>
2025-03-23 20:33:15 -0700

    add session

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -1,20 +1,9 @@
 import * as dao from "./dao.js";
-
-let currentUser = null;
-
 export default function UserRoutes(app) {
   const createUser = (req, res) => { };
   const deleteUser = (req, res) => { };
   const findAllUsers = (req, res) => { };
   const findUserById = (req, res) => { };
-  const updateUser = (req, res) => {
-    const userId = req.params.userId;
-    const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    currentUser = dao.findUserById(userId);
-    res.json(currentUser);
-
-   };
   const signup = (req, res) => { 
     const user = dao.findUserByUsername(req.body.username);
     if (user) {
@@ -22,20 +11,40 @@ export default function UserRoutes(app) {
         { message: "Username already in use" });
       return;
     }
-    currentUser = dao.createUser(req.body);
+    const currentUser = dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
   const signin = (req, res) => { 
     const { username, password } = req.body;
-    currentUser = dao.findUserByCredentials(username, password);
-    res.json(currentUser);
-
+    const currentUser = dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
   };
-  const signout = (req, res) => { };
+
   const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    console.log("Current User", currentUser);
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
+
     res.json(currentUser);
   };
+  const updateUser = (req, res) => {
+    const userId = req.params.userId;
+    const userUpdates = req.body;
+    dao.updateUser(userId, userUpdates);
+    const currentUser = dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
+    res.json(currentUser);
 
+   };
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
@@ -43,6 +52,10 @@ export default function UserRoutes(app) {
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  };
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
 }
