9cf87a050f4114bbec1f9fbee4dbcb122e68bcad
sidrayu <sidra@udel.edu>
2025-03-30 17:31:28 -0700

    debug

--- index.js
+++ index.js
@@ -37,5 +37,9 @@ EnrollmentRoutes(app);
 Lab5(app)
 ModuleRoutes(app);
 Hello(app)
-app.listen(process.env.PORT || 4000)
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+app.use(cors({ origin: true, credentials: true }));
