521da0f0aaaee3afb2fa890ba120a51c4dca3143
sidrayu <sidra@udel.edu>
2025-04-11 21:52:40 -0700

    add users search

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -6,4 +6,13 @@ export const findUserByUsername = (username) =>  model.findOne({ username: usern
 export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
 export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+export const findUsersByPartialName = (partialName) => {
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+  return model.find({
+    $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+  });
+};
+
+
 
