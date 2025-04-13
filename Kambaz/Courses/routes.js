f4d2fb66e636054f7a7f7a3aa7e2d937d7865729
sidrayu <sidra@udel.edu>
2025-04-12 22:04:38 -0700

    add update

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -59,10 +59,10 @@ export default function CourseRoutes(app) {
     const status = await dao.deleteCourse(courseId);
     res.send(status);
   });
-  app.put("/api/courses/:courseId", (req, res) => {
+  app.put("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
-    const status = dao.updateCourse(courseId, courseUpdates);
+    const status = await dao.updateCourse(courseId, courseUpdates);
     res.send(status);
   });
   app.get("/api/courses/:courseId/modules", (req, res) => {
